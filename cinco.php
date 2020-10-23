ba264a32751d23ce085dc4f3d535019a20e86a0a
Utilizador <Utilizador@WIN-MP3JADQD58A.local>
2020-10-23 12:13:04 +0100

    Update cinco.php, dois.php, and 5 more files...

---
+++ cinco.php
@@ -0,0 +1,15 @@
+<?php 
+	$pacientes = array(
+		'Joana' => 20, 
+		'Rui' => 25,
+		'Ana' => 30,
+		'Pedro' => 24,
+		'Carla' => 23
+	);
+
+	echo 'Idade do paciente Joana ' . $pacientes['Joana'] . 'anos';
+
+	foreach ($pacientes as $nome => $idade) {
+		echo 'O paciente ' . $nome . ' tem ' . $idade . 'anos. <br>';
+	}
+?>
